0601e3f26b30117a89a7ab71d511619101edb1c5
Leon King <lking3@my.waketech.edu>
2019-02-05 01:58:07 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -17,5 +17,5 @@ def move(board, index, character = "X")
   def update_array_at_with(array, index, value)
     array[index] = value
   end
-  update_array_at_with(board, 0, "X")
+  move(board, 0, "X")
 end
